9046839182bd1afb621f2048987dfaffe4f6f995
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-05-23 23:58:31 +0900

    listenしわすれ

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ import { MusicBot } from "./bot";
 http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Discord Bot is active now");
-});
+}).listen(8080);
 
 new MusicBot().Run(process.env.TOKEN);
