24f278f32345d8605526f769ba4356e2f391f4d2
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-06-24 17:24:39 +0900

    Update bot.ts
    3e29483 のコミットの修正

--- src/bot.ts
+++ src/bot.ts
@@ -41,7 +41,7 @@ export class MusicBot {
     log("[Main]Main bot is instantiated");
     try{
       this.versionInfo = 
-        execSync("git branch --show-current").toString().trim();
+        execSync("git log -n 1 --pretty=format:%h").toString().trim();
     }
     catch{};
 
