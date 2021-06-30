cf7472937405e14a110ce707905b6c953e968a40
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-06-30 20:09:08 +0900

    関連動画の選別方法の変更

--- src/Component/QueueManager.ts
+++ src/Component/QueueManager.ts
@@ -199,7 +199,9 @@ export class QueueManager extends ManagerBase {
       if(this.info.AddRelative && this.info.Manager.CurrentVideoInfo.ServiceIdentifer === "youtube"){
         const relatedVideos = (this.info.Manager.CurrentVideoInfo as YouTube).relatedVideos;
         if(relatedVideos.length >= 1){
-          await this.info.Queue.AddQueue(relatedVideos[0].url, null, "push", "youtube", relatedVideos[0]);
+          const random = Math.random();
+          const video = relatedVideos[(relatedVideos.length < 2 ? 0 : relatedVideos.length < 3 ? (random >= 0.5 ? 0 : 1) : (random >= 0.33 ? 0 : random >= 0.66 ? 1 : 2))];
+          await this.info.Queue.AddQueue(video.url, null, "push", "youtube", video);
         }
       }
     }
