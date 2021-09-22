2b60ce5fd54711514402227f260800451da146cd
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-09-22 17:41:22 +0900

    Fix: fix #70 / unable to import as library

--- util/preparelib.js
+++ util/preparelib.js
@@ -1,7 +1,8 @@
 const { exec } = require("child_process");
+const path = require("path");
 
 const dirs = __dirname.replace(/\\/g, "/").split("/");
-if(dirs[dirs.length - 2] === "node_modules"){
+if(dirs[dirs.length - 3] === "node_modules"){
   console.log("Preparing library...");
-  exec("npx tsc -p tsconfig.lib.json");
+  exec("npx tsc -p " + path.join(__dirname, "../tsconfig.lib.json"));
 }
