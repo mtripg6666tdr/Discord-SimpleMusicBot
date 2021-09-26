c812a9fd365f9b375eeaaea2b12e4e8c65f79fd8
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-09-26 22:52:27 +0900

    Improve: add documentation comments

--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -9,6 +9,9 @@ import { TaskCancellationManager } from "../Component/TaskCancellationManager";
 import Commands from "./commands";
 import { log } from "../Util";
 
+/**
+ * すべてのコマンドの規定インターフェースです
+ */
 export interface CommandInterface {
   run(message:CommandMessage, options:CommandArgs):Promise<void>;
   name: string;
@@ -21,6 +24,9 @@ export interface CommandInterface {
   argument?:SlashCommandArgument[]
 }
 
+/**
+ * スラッシュコマンドの引数として取れるものを定義するインターフェースです
+ */
 export interface SlashCommandArgument {
   type:"bool"|"integer"|"string",
   name:string,
@@ -28,26 +34,79 @@ export interface SlashCommandArgument {
   required:boolean
 }
 
+/**
+ * コマンドのランナに渡される引数
+ */
 export interface CommandArgs {
+  /**
+   * ボットのインスタンス
+   */
   bot:MusicBot;
+  /**
+   * ボットのサーバーデータ
+   */
   data:{[key:string]:GuildDataContainer};
+  /**
+   * コマンドの生の引数
+   */
   rawArgs: string;
+  /**
+   * コマンドのパース済み引数
+   */
   args: string[];
+  /**
+   * 紐づけチャンネルの更新関数
+   * @param message 更新に使うコマンドメッセージ
+   */
   updateBoundChannel(message:CommandMessage):void;
+  /**
+   * 生存しているPageToggleの配列
+   */
   EmbedPageToggle:PageToggle[];
+  /**
+   * ボットのクライアント
+   */
   client:Client;
+  /**
+   * VC参加関数
+   * @param message 参加に使うメッセージ
+   * @param reply メッセージに返信するかどうか
+   */
   Join(message:CommandMessage, reply?:boolean):Promise<boolean>;
+  /**
+   * URLからキューに追加する関数
+   * @param message 追加に使うメッセージ
+   * @param optiont URL
+   * @param first 最初に追加するかどうか
+   */
   PlayFromURL(message:CommandMessage, optiont:string, first:boolean):Promise<void>;
+  /**
+   * サーバーデータの初期化関数
+   * @param guildid サーバーID
+   * @param channelid チャンネルID
+   */
   initData(guildid:string, channelid:string):void;
+  /**
+   * キャンセルマネージャー
+   */
   cancellations:TaskCancellationManager[];
 }
 
+/**
+ * コマンドマネージャー
+ */
 export class CommandsManager {
   private static _instance = null as CommandsManager;
+  /**
+   * コマンドマネージャーの唯一のインスタンスを返します
+   */
   static get Instance(){
     if(this._instance) return this._instance;
     else return this._instance = new CommandsManager();
   }
+  /**
+   * コマンドマネージャーの唯一のインスタンスを返します
+   */
   get Commands(){
     return this.commands;
   }
@@ -73,6 +132,11 @@ export class CommandsManager {
     log("[CommandsManager]Initialized");
   }
 
+  /**
+   * コマンド名でコマンドを解決します
+   * @param command コマンド名
+   * @returns 解決されたコマンド
+   */
   resolve(command:string){
     log("[CommandsManager]Resolve() called");
     let result = null;
