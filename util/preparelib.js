aab8737a2869c4db9f3f87e7c094cc9af6ca3487
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-10-07 23:48:52 +0900

    Chore: (preparelib) execute escaped path

--- util/preparelib.js
+++ util/preparelib.js
@@ -4,5 +4,5 @@ const path = require("path");
 const dirs = __dirname.replace(/\\/g, "/").split("/");
 if(dirs[dirs.length - 3] === "node_modules"){
   console.log("Preparing library...");
-  exec("npx tsc -p " + path.join(__dirname, "../tsconfig.lib.json"));
+  exec("npx tsc -p \"" + path.join(__dirname, "../tsconfig.lib.json\""));
 }
