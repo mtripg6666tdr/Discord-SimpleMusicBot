5ab17d8a6edd82fe8ae61ed3d1b3611bf445231e
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-10-08 21:26:05 +0900

    Fix:(preparelib) #80

--- util/preparelib.js
+++ util/preparelib.js
@@ -1,8 +1,13 @@
-const { exec } = require("child_process");
+const { execFileSync } = require("child_process");
 const path = require("path");
 
 const dirs = __dirname.replace(/\\/g, "/").split("/");
 if(dirs[dirs.length - 3] === "node_modules"){
   console.log("Preparing library...");
-  exec("npx tsc -p \"" + path.join(__dirname, "../tsconfig.lib.json\""));
+  let commandLine = ["npx", "tsc", "-p", path.join(__dirname, "../tsconfig.lib.json")];
+  if(process.platform === "win32"){
+    commandLine = ["cmd", "/c", ...commandLine];
+  }
+  const executable = commandLine.shift();
+  execFileSync(executable, commandLine);
 }
