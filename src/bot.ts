bc6409ca0b42df2307e470f7ad786a751722019e
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-10-16 23:38:49 +0900

    fix: type check error related to the updating

--- src/bot.ts
+++ src/bot.ts
@@ -113,6 +113,7 @@ export class MusicBot {
                 voice.joinVoiceChannel({
                   channelId: vc.id,
                   guildId: vc.guild.id,
+                  // @ts-ignore
                   adapterCreator: vc.guild.voiceAdapterCreator
                 });
                 await this.data[id].Player.Play();
@@ -450,6 +451,7 @@ export class MusicBot {
         voice.joinVoiceChannel({
           channelId: message.member.voice.channel.id,
           guildId: message.member.guild.id,
+          // @ts-ignore
           adapterCreator: message.member.guild.voiceAdapterCreator,
           debug: Boolean(process.env.DEBUG)
         }).on("debug", (mes) => log("[Connection]" + mes));
