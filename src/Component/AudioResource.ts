cef68d671e5a400406f6a462396e9a921b667a94
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-10-27 20:44:50 +0900

    Improve: judgement of whether the audio resource finished

--- src/Component/AudioResource.ts
+++ src/Component/AudioResource.ts
@@ -2,6 +2,8 @@ import * as voice from "@discordjs/voice";
 
 class NullMetaAudioResource extends voice.AudioResource<null> {};
 
+const SILENCE_FRAME = Buffer.from([0xf8, 0xff, 0xfe]);
+
 export class FixedAudioResource extends NullMetaAudioResource {
   public error = false;
   constructor(...args:ConstructorParameters<typeof NullMetaAudioResource>){
@@ -12,19 +14,44 @@ export class FixedAudioResource extends NullMetaAudioResource {
     });
   }
 
+  private get isStreamReadable() {
+    return !(this.playStream.readableEnded || this.playStream.destroyed || this.error)
+  }
+
   public get readable(){
     if (this.silenceRemaining === 0) return false;
-		const real = !(this.playStream.readableEnded || this.playStream.destroyed || this.error);
-		if (!real) {
-			if (this.silenceRemaining === -1) this.silenceRemaining = this.silencePaddingFrames;
-			return this.silenceRemaining !== 0;
-		}
-		return real;
+    const real = this.isStreamReadable;
+    if (!real) {
+      if (this.silenceRemaining === -1) this.silenceRemaining = this.silencePaddingFrames;
+      return this.silenceRemaining !== 0;
+    }
+    return real;
   }
 
   public get ended() {
-		return this.playStream.readableEnded || this.playStream.destroyed || this.error || this.silenceRemaining === 0;
+    return !this.isStreamReadable && this.silenceRemaining === 0;
 	}
+
+  public read(): Buffer | null {
+    if (this.silenceRemaining === 0) {
+      return null;
+    } else if (this.silenceRemaining > 0) {
+      if (this.isStreamReadable){
+        this.silenceRemaining = -1;
+      } else {
+        this.silenceRemaining--;
+        return SILENCE_FRAME;
+      }
+    }
+    if(!this.playStream.readable){
+      return SILENCE_FRAME;
+    }
+    const packet: Buffer | null = this.playStream.read();
+    if (packet) {
+      this.playbackDuration += 20;
+    }
+    return packet;
+  }
 
   static fromAudioResource(resource:voice.AudioResource){
     return new this(resource.edges, [resource.playStream], null, resource.silencePaddingFrames);
