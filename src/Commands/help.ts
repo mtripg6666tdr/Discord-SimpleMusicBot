c667813980042950b0c09554a64bd2e1a0471d5d
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2021-12-24 23:33:41 +0900

    Fix: Commands: help: not working on certain env
    An user which has not been cached yet can not be obtain by Client.users.resolve.
    Use Client.users.fetch instead of it.

--- src/Commands/help.ts
+++ src/Commands/help.ts
@@ -12,26 +12,28 @@ export default class Help implements CommandInterface {
   category = "bot";
   async run(message:CommandMessage, options:CommandArgs){
     options.updateBoundChannel(message);
-    const embed = new discord.MessageEmbed();
-    embed.title = options.client.user.username + ":notes:";
-    embed.description = "高音質な音楽を再生して、Discordでのエクスペリエンスを最高にするため作られました:robot:\r\n"
-    + "利用可能なコマンドを確認するには、`" + options.data[message.guild.id].PersistentPref.Prefix + "command`を使用してください。";
-    embed.addField("開発者", "[" + options.client.users.resolve("593758391395155978").username + "](https://github.com/mtripg6666tdr)");
-    embed.addField("バージョン", "`" + options.bot.Version + "`");
-    embed.addField("レポジトリ/ソースコード","https://github.com/mtripg6666tdr/Discord-SimpleMusicBot");
-    embed.addField("サポートサーバー", "https://discord.gg/7DrAEXBMHe")
-    embed.addField("現在対応している再生ソース", 
-      "・YouTube(キーワード検索)\r\n"
-    + "・YouTube(動画URL指定)\r\n"
-    + "・YouTube(プレイリストURL指定)\r\n"
-    + "・SoundCloud(キーワード検索)\r\n"
-    + "・SoundCloud(楽曲ページURL指定)\r\n"
-    + "・Streamable(動画ページURL指定)\r\n"
-    + "・Discord(音声ファイルの添付付きメッセージのURL指定)\r\n"
-    + "・Googleドライブ(音声ファイルの限定公開リンクのURL指定)\r\n"
-    + "・オーディオファイルへの直URL"
-    );
-    embed.setColor(getColor("HELP"));
+    const developer = await options.client.users.fetch("593758391395155978").catch(_ => null);
+    const embed = new discord.MessageEmbed()
+      .setTitle(options.client.user.username + ":notes:")
+      .setDescription(
+        "高音質な音楽を再生して、Discordでのエクスペリエンスを最高にするため作られました:robot:\r\n"
+      + "利用可能なコマンドを確認するには、`" + options.data[message.guild.id].PersistentPref.Prefix + "command`を使用してください。")
+      .addField("開発者", `[${(developer || {}).username ?? "mtripg6666tdr"}](https://github.com/mtripg6666tdr)`)
+      .addField("バージョン", "`" + options.bot.Version + "`")
+      .addField("レポジトリ/ソースコード","https://github.com/mtripg6666tdr/Discord-SimpleMusicBot")
+      .addField("サポートサーバー", "https://discord.gg/7DrAEXBMHe")
+      .addField("現在対応している再生ソース", 
+        "・YouTube(キーワード検索)\r\n"
+      + "・YouTube(動画URL指定)\r\n"
+      + "・YouTube(プレイリストURL指定)\r\n"
+      + "・SoundCloud(キーワード検索)\r\n"
+      + "・SoundCloud(楽曲ページURL指定)\r\n"
+      + "・Streamable(動画ページURL指定)\r\n"
+      + "・Discord(音声ファイルの添付付きメッセージのURL指定)\r\n"
+      + "・Googleドライブ(音声ファイルの限定公開リンクのURL指定)\r\n"
+      + "・オーディオファイルへの直URL"
+      )
+      .setColor(getColor("HELP"));
     message.reply({embeds:[embed]}).catch(e => log(e, "error"));
   }
 }
