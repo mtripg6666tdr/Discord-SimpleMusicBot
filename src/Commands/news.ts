b7716a5cb656f1d2beff492731267f5e5aaaf96c
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-01-21 00:09:07 +0900

    Feat: news feature (#119)

---
+++ src/Commands/news.ts
@@ -0,0 +1,86 @@
+import { MessageActionRow, MessageEmbed, MessageSelectMenu, MessageSelectOptionData } from "discord.js";
+import * as ytpl from "ytpl";
+import { CommandArgs, CommandInterface } from ".";
+import { CommandMessage } from "../Component/CommandMessage"
+import { log } from "../Util";
+import { getColor } from "../Util/colorUtil";
+
+export default class News implements CommandInterface {
+  name = "news";
+  alias = ["ニュース"];
+  description = "現在配信されているニューストピックスを閲覧・視聴できます。";
+  unlist = false;
+  category = "playlist";
+  async run(message:CommandMessage, options:CommandArgs){
+    options.updateBoundChannel(message);
+    if(!(await options.JoinVoiceChannel(message))) return;
+    const url = "https://www.youtube.com/playlist?list=PL3ZQ5CpNulQk8-p0CWo9ufI81IdrGoyNZ";
+    if(options.data[message.guild.id].SearchPanel !== null){
+      message.reply("✘既に開かれている検索窓があります").catch(e => log(e, "error"));
+      return;
+    }
+    try{
+      const reply = await message.reply("🔍取得中...");
+      options.data[message.guild.id].SearchPanel = {
+        Msg: {
+          chId: message.channel.id,
+          id: reply.id,
+          userId: message.author.id,
+          userName: message.member.displayName
+        },
+        Opts: {}
+      };
+      const {items: result} = await ytpl.default(url, {
+        gl: "JP", hl: "ja"
+      });
+      let desc = "";
+      const selectOpts = [] as MessageSelectOptionData[];
+      for(let i = 0; i < result.length; i++){
+        const vid = result[i];
+        desc += `\`${i + 1}.\` [${vid.title}](${vid.url}) \`${vid.duration}\` - \`${vid.author.name}\` \r\n\r\n`;
+        options.data[message.guild.id].SearchPanel.Opts[i + 1] = {
+          title: vid.title,
+          url: vid.url,
+          duration: vid.duration,
+          thumbnail: vid.thumbnails[0].url
+        };
+        selectOpts.push({
+          label: `${i}. ${vid.title.length > 90 ? vid.title.substring(0, 90) : vid.title}`,
+          description: `長さ: ${vid.duration}, チャンネル名: ${vid.author.name}`,
+          value: (i + 1).toString()
+        });
+      }
+      const embed = new MessageEmbed()
+        .setTitle("ニューストピックス")
+        .setDescription(desc)
+        .setColor(getColor("SEARCH"))
+        .setFooter({
+          iconURL: message.author.avatarURL(),
+          text:"ニュースのタイトルを選択して数字を送信してください。キャンセルするにはキャンセルまたはcancelと入力します。"
+        })
+      ;
+      await reply.edit({
+        content: null,
+        embeds: [embed],
+        components: [
+          new MessageActionRow()
+          .addComponents(
+            new MessageSelectMenu()
+            .setCustomId("search")
+            .setPlaceholder("数字を送信するか、ここから選択...")
+            .setMinValues(1)
+            .setMaxValues(result.length)
+            .addOptions([...selectOpts, {
+              label: "キャンセル",
+              value: "cancel"
+            }])
+          )
+        ]
+      });
+    }
+    catch(e){
+      log(JSON.stringify(e));
+      message.reply(":cry:エラーが発生しました");
+    }
+  }
+}
