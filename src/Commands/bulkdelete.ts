5f0ac1d1c007884c594f20babb808193b20a84be
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-05-07 20:52:26 +0900

    create bulk delete command file

---
+++ src/Commands/bulkdelete.ts
@@ -0,0 +1,17 @@
+import { CommandArgs, BaseCommand } from ".";
+import { CommandMessage } from "../Component/CommandMessage"
+
+export default class Bgm extends BaseCommand {
+  constructor(){
+    super({
+      name: "バルク削除",
+      alias: ["bulk-delete", "bulkdelete"],
+      description: "ボットが送信したメッセージを一括削除します。",
+      unlist: false,
+      category: "playlist",
+    });
+  }
+  
+  async run(message:CommandMessage, options:CommandArgs){
+  }
+}
