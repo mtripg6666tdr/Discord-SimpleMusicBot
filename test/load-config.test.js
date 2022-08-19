259eae3f8266cb71a34ff2f3c91e53381d0da924
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-08-19 17:28:39 +0900

    log progress of test [wip]

--- test/load-config.test.js
+++ test/load-config.test.js
@@ -2,11 +2,16 @@
 const assert = require("assert");
 const fs = require("fs");
 const path = require("path");
+
 const configPath = path.join(__dirname, "../config.json");
+console.log(`Config path: ${configPath}`);
 const alreadyExists = fs.existsSync(configPath);
+console.log(alreadyExists ? "Config file exists" : "Config file not found");
 const original = alreadyExists ? fs.readFileSync(configPath, {encoding: "utf-8"}) : null;
+alreadyExists && console.log("Reading original config");
 if(alreadyExists){
   fs.copyFileSync(configPath, path.join(__dirname, "../config.backup.temp.json"));
+  console.log("Made backup of the original config file. If the config file is left overwritten, please remove config.json and rename config.backup.temp.json into config.json.");
 }
 const sampleJson = `
 // Test is in progress now. Please do not modify this file
@@ -26,6 +31,7 @@ const sampleJson = `
   "prefix": "<" // デフォルトプレフィックス、一文字。省略されたりnullなら\`>\`
 }`;
 fs.writeFileSync(configPath, sampleJson, {encoding: "utf-8"});
+console.log("Successfully wrote the config for test");
 const configLoaderPath = "../dist/Util/config"
 
 describe("Config", function() {
@@ -58,7 +64,9 @@ describe("Config", function() {
 process.on("exit", () => {
   if(original){
     fs.writeFileSync(configPath, original, {encoding: "utf-8"});
+    console.log("Config file was restored successfully");
   }else{
     fs.unlinkSync(configPath);
+    console.log("Config file was removed successfully");
   }
 });
