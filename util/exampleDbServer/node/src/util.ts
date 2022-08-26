bf211b32aabd8d341d88e35601fe8391e059ec74
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-08-26 21:38:12 +0900

    Feat: example db server for node

--- util/exampleDbServer/node/src/util.ts
+++ util/exampleDbServer/node/src/util.ts
@@ -1,5 +1,5 @@
-export function parseQuery(url:string){
-  return Object.assign({}, ...(url.substring(2).split("&").map(q => q.split("=")).map(qs => ({[decodeURIComponent(qs[0])]:decodeURIComponent(qs[1])}))));
+export function parseQuery(url:string):{[key:string]:string}{
+  return Object.assign({}, ...(url.split("&").map(q => q.split("=")).map(qs => ({[decodeURIComponent(qs[0])]:decodeURIComponent(qs[1])}))));
 }
 
 export type GuildData = {[guildId:string]:string};
