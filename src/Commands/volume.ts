345f880e83d7317384820cb0542ac1b4d41aef8d
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2022-08-27 15:37:13 +0900

    Fix(command/volume): support for slash command

--- src/Commands/volume.ts
+++ src/Commands/volume.ts
@@ -14,25 +14,31 @@ export default class Volume extends BaseCommand {
       category: "voice",
       examples: "volume [音量]",
       usage: "volume 120",
+      argument: [{
+        type: "integer",
+        name: "volume",
+        description: "変更先の音量。20~200までが指定できます。",
+        required: false
+      }]
     });
   }
 
   async run(message:CommandMessage, options:CommandArgs){
     options.server.updateBoundChannel(message);
     if(options.rawArgs === ""){
-      await message.channel.createMessage(`:loud_sound:現在の音量は**${options.server.player.volume}**です(デフォルト:100)`)
+      await message.reply(`:loud_sound:現在の音量は**${options.server.player.volume}**です(デフォルト:100)`)
         .catch(e => Util.logger.log(e, "error"))
       ;
       return;
     }
     const newval = Number(options.rawArgs);
     if(isNaN(newval) || newval < 1 || newval > 200){
-      message.channel.createMessage(":bangbang:音量を変更する際は1から200の数字で指定してください。")
+      message.reply(":bangbang:音量を変更する際は1から200の数字で指定してください。")
         .catch(e => Util.logger.log(e, "error"));
       return;
     }
     const result = options.server.player.setVolume(newval);
-    await message.channel.createMessage(`:loud_sound:音量を**${newval}**に変更しました。\r\n${!result ? "次の曲から適用されます。現在再生中の曲に設定を適用するには、`頭出し`コマンドなどを使用してください。" : ""}`)
+    await message.reply(`:loud_sound:音量を**${newval}**に変更しました。\r\n${!result ? "次の曲から適用されます。現在再生中の曲に設定を適用するには、`頭出し`コマンドなどを使用してください。" : ""}`)
       .catch(e => Util.logger.log(e, "error"));
   }
 }
