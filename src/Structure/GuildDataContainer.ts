b9c365a88dd4eb44b7085f8fdf95286c5b279317
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-03-01 15:22:43 +0900

    Feat: more customizable prefix

--- src/Structure/GuildDataContainer.ts
+++ src/Structure/GuildDataContainer.ts
@@ -554,9 +554,9 @@ export class GuildDataContainer extends LogEmitter {
     const guild = "guild" in message ? message.guild : message.channel.guild;
     const oldPrefix = this.prefix;
     const member = guild.members.get(this.bot.client.user.id);
-    const pmatch = (member.nick || member.username).match(/^\[(?<prefix>.)\]/);
+    const pmatch = (member.nick || member.username).match(/^(\[(?<prefix0>[a-zA-Z!?-_]+)\]|【(?<prefix1>[a-zA-Z!?-_]+)】)/);
     if(pmatch){
-      if(this.prefix !== pmatch.groups.prefix){
+      if(this.prefix !== (pmatch.groups.prefix0 || pmatch.groups.prefix1)){
         this.prefix = Util.string.NormalizeText(pmatch.groups.prefix);
       }
     }else if(this.prefix !== Util.config.prefix){
