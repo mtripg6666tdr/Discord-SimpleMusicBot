90650b99d951018955637dc7dfa7d00649bf52d7
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-03-06 19:46:55 +0900

    Chore: fix ci

--- test/load-config.test.js
+++ test/load-config.test.js
@@ -18,7 +18,7 @@ const sampleJson = `
 {
   "adminId": "123456", // 管理ユーザーのユーザーID (任意)、なければnull
 
-  "debug": false, // デバッグ可能かどうか
+  "debug": true, // デバッグ可能かどうか
   
   "maintenance": true, // メンテナンス中かどうか
   // ボットの操作を全封鎖するときのみ設定してください。
