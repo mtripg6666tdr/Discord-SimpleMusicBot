4e714df8558b1753ccba8d1cee0edf8cc944cda2
nh-chitose <50246835+nh-chitose@users.noreply.github.com>
2023-04-21 14:03:01 +0900

    Fix: const UA as UA

--- test/spotifylink.js
+++ test/spotifylink.js
@@ -1,22 +1,18 @@
 async function expandSpotify(url) {
+  const UA =  {
+    "mode": "cors",
+    "headers": {
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36"
+    }
+  };
   /**
    * 1段階目のリダイレクトを取得
    */
-  const firstRedirect = await fetch(url, {
-    "mode": "cors",
-    "headers": {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36"
-    }
-  });
+  const firstRedirect = await fetch(url, UA);
   /**
    * 2段階目のリダイレクトを取得
    */
-  const spotifyPage = await fetch(firstRedirect.url, {
-    "mode": "cors",
-    "headers": {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36"
-    }
-  });
+  const spotifyPage = await fetch(firstRedirect.url, UA);
   console.log("Console: " + spotifyPage.url);
   return spotifyPage.url;
 }
