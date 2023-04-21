0e63746d24220361aa3d5d310303958b449655db
nh-chitose <50246835+nh-chitose@users.noreply.github.com>
2023-04-21 14:01:03 +0900

    Feat: fetch spotify URL

---
+++ test/spotifylink.js
@@ -0,0 +1,26 @@
+async function expandSpotify(url) {
+  /**
+   * 1段階目のリダイレクトを取得
+   */
+  const firstRedirect = await fetch(url, {
+    "mode": "cors",
+    "headers": {
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36"
+    }
+  });
+  /**
+   * 2段階目のリダイレクトを取得
+   */
+  const spotifyPage = await fetch(firstRedirect.url, {
+    "mode": "cors",
+    "headers": {
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36"
+    }
+  });
+  console.log("Console: " + spotifyPage.url);
+  return spotifyPage.url;
+}
+
+const result = expandSpotify("https://spotify.link/hOw0HcMUXyb");
+
+console.log("Result: " + result);
