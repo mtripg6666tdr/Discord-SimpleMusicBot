887869a2004be9d04d0c78dec9c2dcbae13d9953
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-12-18 19:25:35 +0900

    Improve(commands/bgm): hardcoded item-list for efficiency (#2155)
    * Improve(commands/bgm): hardcoded item-list for efficiency
    
    * fix building
    
    * fix linting

--- src/Commands/bgm.ts
+++ src/Commands/bgm.ts
@@ -20,12 +20,7 @@ import type { CommandArgs } from ".";
 import type { CommandMessage } from "../Component/commandResolver/CommandMessage";
 import type { i18n } from "i18next";
 
-import * as ytpl from "ytpl";
-
 import { BaseCommand } from ".";
-import { useConfig } from "../config";
-
-const config = useConfig();
 
 export default class Bgm extends BaseCommand {
   constructor(){
@@ -45,8 +40,6 @@ export default class Bgm extends BaseCommand {
     // attempt to join
     if(!await context.server.joinVoiceChannel(message, { replyOnFail: true }, t)) return;
 
-    const url = "https://www.youtube.com/playlist?list=PLLffhcApso9xIBMYq55izkFpxS3qi9hQK";
-
     // check existing search panel
     if(context.server.searchPanel.has(message.member.id)){
       message.reply(t("search.alreadyOpen")).catch(this.logger.error);
@@ -63,11 +56,8 @@ export default class Bgm extends BaseCommand {
       return;
     }
     await searchPanel.consumeSearchResult(
-      ytpl.default(url, {
-        gl: config.country,
-        hl: context.locale,
-      }),
-      ({ items }) => items.map(item => ({
+      Promise.resolve(bgms),
+      items => items.map(item => ({
         title: item.title,
         author: item.author.name,
         description: `${
@@ -83,3 +73,148 @@ export default class Bgm extends BaseCommand {
     );
   }
 }
+
+type BgmItem = {
+  title: string,
+  author: {
+    name: string,
+  },
+  duration: string,
+  thumbnails: [{
+    url: string,
+  }],
+  url: string,
+};
+
+const bgms: BgmItem[] = JSON.parse(`[
+  {
+    "title": "【勉強用・作業用BGM】α波で超集中・記憶力アップ【波の音×オルゴール】",
+    "author": {
+      "name": "Stardy -河野玄斗の神授業"
+    },
+    "duration": "2:00:00",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/vr9dLvJs7VE/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=vr9dLvJs7VE"
+  },
+  {
+    "title": "カノン1時間【勉強用・作業用・睡眠用BGM】ピアノ/パッヘルベル/楽譜あり/Canon1hour/Pachelbel /Piano/Instrument Music/CANACANA",
+    "author": {
+      "name": "CANACANA family"
+    },
+    "duration": "1:02:39",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/ZrmqqFUVT8Y/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=ZrmqqFUVT8Y"
+  },
+  {
+    "title": "１時間が１５分に感じる超集中 Study Music【勉強集中音楽BGM】",
+    "author": {
+      "name": "Study JaPan Bmg"
+    },
+    "duration": "1:01:28",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/ivuUUE1B4cU/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=ivuUUE1B4cU"
+  },
+  {
+    "title": "【作業用 - 勉強用BGM】集中 | 記憶力を向上させる！ヒーリングピアノ曲集 - Study With Me",
+    "author": {
+      "name": "Japan Study Music "
+    },
+    "duration": "1:04:57",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/tTB8uoB0I5M/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=tTB8uoB0I5M"
+  },
+  {
+    "title": "ディズニーピアノメドレー【作業用、勉強、睡眠用BGM】Disney Piano Medley for Studying and Sleeping (Piano Covered by kno)",
+    "author": {
+      "name": "kno Disney Piano Channel"
+    },
+    "duration": "2:30:36",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/LzhP4S7gV5w/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=LzhP4S7gV5w"
+  },
+  {
+    "title": "ディズニーピアノメドレーVol.2【作業用、勉強、睡眠用BGM】Disney Piano Medley for Studying and Sleeping",
+    "author": {
+      "name": "kno Disney Piano Channel"
+    },
+    "duration": "3:31:04",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/O0Sd37i9AEc/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=O0Sd37i9AEc"
+  },
+  {
+    "title": "【雨の音】超集中できる自然音。ポモドーロタイマー６セット｜（25分作業×5分休憩）×6セット【ポモドーロテクニック/作業用BGM/仕事用BGM/勉強用BGM】",
+    "author": {
+      "name": "マインドキング"
+    },
+    "duration": "2:57:03",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/UFiztcMoKa8/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=UFiztcMoKa8"
+  },
+  {
+    "title": "【癒し系】ボカロピアノアレンジ【作業用BGM】",
+    "author": {
+      "name": "EMEProject"
+    },
+    "duration": "29:52",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/BB-MyQU6URs/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=BB-MyQU6URs"
+  },
+  {
+    "title": "【合格する音楽】サブリミナル効果、合格祈願、勉強、テスト前日、引き寄せの法則、記憶力、受験勉強、ソルフェジオ周波数、音源",
+    "author": {
+      "name": "癒しのBGM"
+    },
+    "duration": "1:00:17",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/qGG04UK-DXA/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=qGG04UK-DXA"
+  },
+  {
+    "title": "夢を撃ち抜け！受験勉強用BGM！2022",
+    "author": {
+      "name": "バンドリちゃんねる☆"
+    },
+    "duration": "1:04:13",
+    "thumbnails": [
+      {
+        "url": "https://i.ytimg.com/vi/ZLWtxQsmD50/hqdefault.jpg"
+      }
+    ],
+    "url": "https://www.youtube.com/watch?v=ZLWtxQsmD50"
+  }
+]`);
