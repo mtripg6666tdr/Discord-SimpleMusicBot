801719860235cf72d51972881e75687fb3a60833
うさみょん/myon2019 <56076195+mtripg6666tdr@users.noreply.github.com>
2024-06-21 21:32:04 +0900

    Chore: update license year (#2413)
    * Chore: update license year
    
    * fix: linting

--- src/Structure/ServerManagerBase.ts
+++ src/Structure/ServerManagerBase.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2023 mtripg6666tdr
+ * Copyright 2021-2024 mtripg6666tdr
  * 
  * This file is part of mtripg6666tdr/Discord-SimpleMusicBot. 
  * (npm package name: 'discord-music-bot' / repository url: <https://github.com/mtripg6666tdr/Discord-SimpleMusicBot> )
