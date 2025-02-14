0bd7d73c321231f70e7f21ac7a96ec241234c6af
うさみょん/myon2019 <56076195+mtripg6666tdr@users.noreply.github.com>
2025-02-15 00:52:38 +0900

    Fix: corrupted radio feature

---
+++ lib/yt-mix-playlist/dist/cjs/Constants.d.ts
@@ -0,0 +1,4 @@
+export declare const BASE_URL = "https://www.youtube.com/";
+export declare const WATCH_URL: string;
+export declare const CONTINUATION_URL: string;
+//# sourceMappingURL=Constants.d.ts.map
---
+++ lib/yt-mix-playlist/dist/mjs/entities/MixPlaylistBasicInfo.d.ts
@@ -0,0 +1,10 @@
+export default class MixPlaylistBasicInfo {
+    id: string;
+    title: string;
+    /** @deprecated */
+    author: string;
+    /** @deprecated */
+    url: string | null;
+    constructor(data: any);
+}
+//# sourceMappingURL=MixPlaylistBasicInfo.d.ts.map
